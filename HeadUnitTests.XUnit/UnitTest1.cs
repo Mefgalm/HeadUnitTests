c5382411aa917c15fcfa5bc4a73d7223b8affbcf
DESKTOP-6Q7D9GK\mefga <mefgalm@gmail.com>
2018-03-25 16:03:21 +0300

    add xunit and xunit logger

---
+++ HeadUnitTests.XUnit/UnitTest1.cs
@@ -0,0 +1,20 @@
+using System;
+using Xunit;
+
+namespace HeadUnitTests.XUnit
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+        
+        
+        [Fact]
+        public void Test2()
+        {
+            Assert.True(true);
+        }
+    }
+}
